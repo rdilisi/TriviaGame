f855372dd210611d8a1b544ee8cd8bbcaf0bf6ac
rdilisi <robertdilisi76@gmail.com>
2018-07-31 18:47:34 -0400

    updating files

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+function check(){
+    alert("Your almost a made guy!!")
+}
+
+var audioElement = document.createElement("audio");
+        audioElement.setAttribute("src", "assets/godfatherthemesong.mp3");
